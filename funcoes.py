ec6e7e03f30a9abaa7706ece3c376105ebfd64b2
Marcelo Marchetto <marcelomarchetto@MacBook-Pro-de-Marcelo.local>
2022-04-06 16:38:18 -0300

    first commit

---
+++ funcoes.py
@@ -0,0 +1,59 @@
+
+import unicodedata
+
+def remove_acentos(s):
+    return ''.join(c for c in unicodedata.normalize('NFD', s)
+                  if unicodedata.category(c) != 'Mn')
+
+def valida_palavra(palavra):     
+    return remove_acentos(palavra.lower())
+
+
+def frequencia_letras(palavra):
+       
+    frequencia_letras = {}
+    for letra in palavra:
+        if letra not in frequencia_letras:
+            frequencia_letras[letra] = 1
+        else:
+            frequencia_letras[letra] += 1
+    return frequencia_letras
+
+
+def letras_certas(chute, gabarito):
+
+    letras_utilizadas = {}
+    
+    gabarito_puro = gabarito
+    gabarito_limpo = valida_palavra(gabarito)
+    
+
+    frequencia_letras_do_gabarito = frequencia_letras(gabarito_limpo)
+    
+
+    nova_palavra = [" ", " "," "," "," "," "]
+    
+    # Letras certas, nos lugares certo
+    for indice, letra in enumerate(chute):
+        
+        if letra == gabarito_limpo[indice]:
+
+            if letra not in letras_utilizadas:
+                letras_utilizadas[letra] = 1
+            else:
+                letras_utilizadas[letra] += 1
+
+            nova_palavra[indice] = gabarito_puro[indice].upper()
+            frequencia_letras_do_gabarito[letra] -= 1
+
+            if gabarito_limpo.count(letra) == letras_utilizadas[letra]:
+                frequencia_letras_do_gabarito[letra] = 0
+
+    for indice, letra in enumerate(chute):
+
+        if letra in gabarito_limpo and frequencia_letras_do_gabarito[letra] > 0:
+            nova_palavra[indice] = chute_teste[indice]
+            frequencia_letras_do_gabarito[letra] -= 1
+
+    
+    return "".join(nova_palavra).replace(" ", "_")
---
+++ teste.py
@@ -0,0 +1,32 @@
+from funcoes import letras_certas
+
+# ----------------------------------------------------------
+
+chute_teste = "iooooo"
+gabarito_teste = "ínsper"
+
+x = letras_certas(chute_teste, gabarito_teste)
+assert x == "Í_____"
+
+# ----------------------------------------------------------
+
+chute_teste = "iieiii"
+gabarito_teste = "eiooei"
+
+x = letras_certas(chute_teste, gabarito_teste)
+assert x == "_Ie__I"
+
+# ----------------------------------------------------------
+
+chute_teste = "insper"
+gabarito_teste = "Ínsper"
+
+x = letras_certas(chute_teste, gabarito_teste)
+assert x == "ÍNSPER"
+
+# ----------------------------------------------------------
+chute_teste = "qqeiei"
+gabarito_teste = "eíntei"
+
+x = letras_certas(chute_teste, gabarito_teste)
+# assert x == "__eiEI"
