6fe74fd4ce86d5b211891840fdfe13e5723a7996
Marcelo Marchetto <marcelomarchetto@MacBook-Pro-de-Marcelo.local>
2022-04-11 08:39:39 -0300

    word generator

---
+++ teste.py
@@ -0,0 +1,19 @@
+import requests
+
+# url = "https://www.ime.usp.br/~pf/dicios/br-utf8.txt"
+
+# page = requests.get(url)
+
+# cinco_letras = []
+# palavras = page.text.split()
+
+# for palavra in palavras:
+#     if len(palavra) == 5:
+#         cinco_letras.append(palavra)
+
+# with open("palavras.txt", "w") as f:
+#     for palavra in cinco_letras:
+#         f.write(palavra)
+#         f.write('\n')
+    
+
